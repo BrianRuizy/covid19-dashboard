fab365065c9e68ba48bd81c5d6840051806614a8
BrianRuizy <brianruiz123@yahoo.com>
2020-03-17 02:48:11 -0500

    added other time series file urls

--- core/process/getdata.py
+++ core/process/getdata.py
@@ -4,8 +4,13 @@ import numpy as np
 import pandas as pd
 import datetime
 
-# Official JHU CSSE data repository
+# Official general JHU CSSE data repository
 daily_report_dir = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'
+
+# Time series files
+confirmed_time_series = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
+deaths_time_series = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv'
+recovered_time_series = 'https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv'
 
 
 def file_date():
@@ -18,7 +23,9 @@ def file_date():
 
 
 def daily_report(date_string = None):
+    #TODO: Implement date_string arg handling for dynamic daily reports
     raw_data = pd.read_csv(daily_report_dir + file_date())
     df = pd.DataFrame(raw_data)
-    
+
     return df
+
