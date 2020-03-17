4ad9ceded8cb0a7969156c60bb250e80905238bf
BrianRuizy <brianruiz123@yahoo.com>
2020-03-17 03:19:10 -0500

    implemented methods for data frame acquisition

--- core/process/getdata.py
+++ core/process/getdata.py
@@ -4,14 +4,6 @@ import numpy as np
 import pandas as pd
 import datetime
 
-# Official general JHU CSSE data repository
-daily_report_dir = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'
-
-# Time series files
-confirmed_time_series = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
-deaths_time_series = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv'
-recovered_time_series = 'https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv'
-
 
 def file_date():
     # Returns file_date of most recent date
@@ -24,8 +16,28 @@ def file_date():
 
 def daily_report(date_string = None):
     #TODO: Implement date_string arg handling for dynamic daily reports
-    raw_data = pd.read_csv(daily_report_dir + file_date())
-    df = pd.DataFrame(raw_data)
+    daily_report_dir = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'
+    df = pd.read_csv(daily_report_dir + file_date())
+    
+    return df
+
+
+def confirmed_report():
+    confirmed_time_series = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
+    df = pd.read_csv(confirmed_time_series)
 
     return df
 
+
+def deaths_report():
+    deaths_time_series = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv'
+    df = pd.read_csv(deaths_time_series)
+    
+    return df
+
+
+def recovered_report():
+    recovered_time_series = 'https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv'
+    df = pd.read_csv(recovered_time_series)
+    
+    return df 
