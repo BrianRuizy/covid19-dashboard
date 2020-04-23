90fd73d250c87d606b1107249609aaa72216fe71
BrianRuizy <brianruiz123@yahoo.com>
2020-04-23 01:01:52 -0500

    v0.1 - Depracated user profiles and authentication

--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,6 @@ from django.urls import path, include  # add this
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('authentication.urls')),  # add this
     path('', include('processdata.urls')),
     path('', include('app.urls')),  # add this
 ]
