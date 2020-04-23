3717c8b43fe2b73587d06ac825245960797858bb
Brian Ruiz <brianruiz123@yahoo.com>
2020-04-23 01:04:53 -0500

    Revert "v0.1 - Depracated user profiles and authentication"

--- processdata/views.py
+++ processdata/views.py
@@ -1,4 +1,4 @@
-
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.http import HttpResponse
