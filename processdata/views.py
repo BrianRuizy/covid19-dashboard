2d60bd40da2074259949cbe8610c157dfa4efbb6
Rahmsauce <familiehn.leo@gmail.com>
2020-09-04 21:23:53 +0200

    Removed no longer necessary prints

--- processdata/views.py
+++ processdata/views.py
@@ -81,11 +81,7 @@ def daily_growth(request):
 def daily_report(request):
     df = getdata.daily_report()
 
-    print(df)
-    
     df.drop(['FIPS', 'Admin2', 'Province_State', 'Country_Region', 'Last_Update', 'Deaths', 'Recovered', 'Active', 'Incidence_Rate', 'Case-Fatality_Ratio'], axis=1, inplace=True)
-
-    print(df)
 
     return HttpResponse(df.to_json(orient='columns'), content_type='application/json')
 
